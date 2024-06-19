210251a55014d3ef815f92ac1faa26fc93f72e6f
TinFoiKa <117747442+TinFoiKa@users.noreply.github.com>
2024-06-19 10:05:58 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"sas-2023if-6gnixjwic1713458","alias":"sas-2023if"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\WeixinDevTools\cli.bat" cloud functions deploy --e sas-2023if-6gnixjwic1713458 --n quickstartFunctions --r --project "C:\Users\Aaron Li\WeChatProjects\IF2425" --report_first --report
